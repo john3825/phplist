5bf8b87e986d3c8814b3b99c2c364f1140583201
yama <yamamoto@kyms.ne.jp>
2013-01-20 16:42:40 +0900

    翻訳調整

--- public_html/lists/admin/help/en/message.php
+++ public_html/lists/admin/help/en/message.php
@@ -2,7 +2,7 @@ In the message field you can use "variables", which will be replaced by the appr
 <br />The variables need to be in the form <b>[NAME]</b> where NAME can be replaced with the name of one of your attributes.
 <br />For example if you have an attribute "First Name" put [FIRST NAME] in the message somewhere to identify the location where the "First Name" value of the recipient needs to be inserted.
 </p><p>Currently you have the following attributes defined:
-<table border=1><tr><td><b>Attribute</b></td><td><b>Placeholder</b></td></tr>
+<table class="grid"><tr><td><b>Attribute</b></td><td><b>Placeholder</b></td></tr>
 <?php
 $req = Sql_query("select name from {$tables["attribute"]} order by listorder");
 while ($row = Sql_Fetch_Row($req))
--- public_html/lists/admin/info/ja/configure.php
+++ public_html/lists/admin/info/ja/configure.php
@@ -1,17 +1,15 @@
-<p class="information">ウェブサイトの正しい値を設定してください。<br/>
-値の中には<b>プレースホルダー</b>を含めることができます。
-プレースホルダーは<b>[SOMETEXT]</b>のように見え、<i>SOMETEXT</i>とは異なっています。.
-いくつかの役に立つプレースホルダー:
+<p class="information">
+<b>[プレースホルダー]</b>を用いることができます。
 </p>
 <div class="placeholders">
 <ul>
-<li>WEBSITE - ウェブサイトのアドレス
-<li>DOMAIN - ウェブサイトのドメイン
-<li>SUBSCRIBEURL - 購読ページのロケーション
-<li>UNSUBSCRIBEURL - 購読解除ページのロケーション
-<li>BLACKLISTURL - the location of the unsubscribe page for unknown users</li>
-<li>PREFERENCESURL - ユーザが詳細を更新することができるページのロケーション
-<li>CONFIRMATIONURL - ユーザが購読を確定しなければいけないページのロケーション
+<li>[WEBSITE] - ウェブサイトのアドレス
+<li>[DOMAIN] - ウェブサイトのドメイン
+<li>[SUBSCRIBEURL] - 購読ページのロケーション
+<li>[UNSUBSCRIBEURL] - 購読解除ページのロケーション
+<li>[BLACKLISTURL] - the location of the unsubscribe page for unknown users</li>
+<li>[PREFERENCESURL] - ユーザが詳細を更新することができるページのロケーション
+<li>[CONFIRMATIONURL] - ユーザが購読を確定しなければいけないページのロケーション
 </ul>
 </div>
 <!--For the header and the footer you can use the following code to include external documents:
